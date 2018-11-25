3ccdc3b810ebaf62277a6ba4b7a7a4fdf700bae0
Deven Reilly <dr@omnipresent.pro>
2018-11-25 16:45:13 -0500

    Created HitRect Objects

---
+++ Assets/Scripts/HitHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitHandler : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "HitCube"){
+            Debug.Log("Collided");
+        }
+    }
+
+    // Use this for initialization
+    void Start(){
+
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log("You selected the " + hit.transform.name);
+            }
+        }
+    }
+}
